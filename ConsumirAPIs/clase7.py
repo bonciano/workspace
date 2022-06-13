f5fc8a4c1bc7aa8f30d879393cbad7c3b38752b4
oterl <95692457+oterl@users.noreply.github.com>
2022-06-13 14:27:22 -0300

    Delete ConsumirAPIs directory

--- ConsumirAPIs/clase7.py
+++
@@ -1,8 +0,0 @@
-# Taller consumir APIs con Python - Chunks
-import requests
-import json
-
-if __name__ == '__main__':
-    url = 'https://wallpapers.com/images/high/angry-sea-dragon-hiiv4o2mkpu7x492.jpg'
-    
-    response = requests.get(url, stream=True)
